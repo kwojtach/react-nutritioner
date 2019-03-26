f13c326084fe82b54f24b9323560b7ddbfe5df11
Krzysiek <krzywoj55@gmail.com>
2019-03-26 21:42:38 +0100

    table component added, proximates and minerals tables moved to new component

--- src/containers/FoodDescription/FoodDetails/FoodDetails.js
+++ src/containers/FoodDescription/FoodDetails/FoodDetails.js
@@ -1,42 +1,22 @@
 import React from 'react';
+import Table from './Table/Table';
 
 import classes from './FoodDetails.scss';
 
 const foodDetails = (props) => {
   return (
     <>
-      <table className={classes.FoodDetails}>
-        <thead>
-          <tr>
-            <th>Proximates</th>
-            <th>Value per 100g</th>
-          </tr>
-        </thead>
-        <tbody>
-        { props.details.proximates.map(proximate =>
-          <tr key={proximate.nutrient_id}>
-            <td>{proximate.name} ({proximate.name.slice(0,1)})</td>
-            <td>{proximate.value} {proximate.unit}</td>
-          </tr>) }
-        </tbody>
-      </table>
-      <table className={classes.FoodDetails}>
-        <thead>
-          <tr>
-            <th>Minerals</th>
-            <th>Value per 100g</th>
-          </tr>
-        </thead>
-        <tbody>
-        { props.details.minerals.map(mineral =>
-          <tr key={mineral.nutrient_id}>
-            <td>{mineral.name} ({mineral.name.slice(0,1)})</td>
-            <td>{mineral.value} {mineral.unit}</td>
-          </tr>) }
-        </tbody>
-      </table>
+      <Table
+        elements={props.details.proximates}
+        title={"Proximates"}
+        tableClass={classes.FoodDetails}
+      />
+      <Table
+        elements={props.details.minerals}
+        title={"Minerals"}
+        tableClass={classes.FoodDetails}
+      />
     </>
-
   )
 };
 
