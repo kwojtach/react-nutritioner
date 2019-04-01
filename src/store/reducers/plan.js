de676fb3df9fb6c9f83602f90d38c32c3e92893d
Krzysiek <krzywoj55@gmail.com>
2019-04-01 21:40:04 +0200

    fixed a bug when adding the same food to the food plan

--- src/store/reducers/plan.js
+++ src/store/reducers/plan.js
@@ -7,6 +7,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case (actionTypes.ADD_FOOD_TO_PLAN):
+      if (state.foodPlan.find(food => food.id === action.food.id)) {
+        return state
+      }
+
       const food = {
         id: action.food.id,
         name: action.food.name,
@@ -21,10 +25,9 @@ const reducer = (state = initialState, action) => {
       };
     case (actionTypes.CALCULATE_FOOD):
       const foodCalculated = state.foodPlan.find(food => food.id === action.foodId);
-      if (action.newWeight > 0) {
-        foodCalculated.weight = action.newWeight;
-      } else return {...state };
+      if (!action.newWeight > 0) return state;
 
+      foodCalculated.weight = action.newWeight;
       foodCalculated.proximates = foodCalculated.proximates.map((proximate, index) => {
         return (Math.ceil(parseFloat(foodCalculated.proximatesValues[index]) * parseFloat(action.newWeight))) / 100
       });
